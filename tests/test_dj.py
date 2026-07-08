ff5376b4320fd46b4957079ca3898026bf67a780
c2keesey <c2keesey@gmail.com>
2026-07-07 20:27:31 -0700

    feat: camelot key mapping for dj module

---
+++ tests/test_dj.py
@@ -0,0 +1,32 @@
+"""Tests for pure DJ math: Camelot mapping, compatibility, energy parsing."""
+
+import pytest
+
+from spotify_dl import dj
+
+
+# ---- camelot ----
+
+@pytest.mark.parametrize("key,code", [
+    ("Abm", "1A"), ("G#m", "1A"), ("B", "1B"),
+    ("Ebm", "2A"), ("D#m", "2A"), ("F#", "2B"), ("Gb", "2B"),
+    ("Bbm", "3A"), ("A#m", "3A"), ("Db", "3B"), ("C#", "3B"),
+    ("Fm", "4A"), ("Ab", "4B"), ("G#", "4B"),
+    ("Cm", "5A"), ("Eb", "5B"), ("D#", "5B"),
+    ("Gm", "6A"), ("Bb", "6B"), ("A#", "6B"),
+    ("Dm", "7A"), ("F", "7B"),
+    ("Am", "8A"), ("C", "8B"),
+    ("Em", "9A"), ("G", "9B"),
+    ("Bm", "10A"), ("D", "10B"),
+    ("F#m", "11A"), ("Gbm", "11A"), ("A", "11B"),
+    ("Dbm", "12A"), ("C#m", "12A"), ("E", "12B"),
+])
+def test_camelot_all_keys(key, code):
+    assert dj.to_camelot(key) == code
+
+
+def test_camelot_unknown_and_none():
+    assert dj.to_camelot(None) is None
+    assert dj.to_camelot("") is None
+    assert dj.to_camelot("H#m") is None
+    assert dj.to_camelot(" Am ") == "8A"      # tolerates whitespace
